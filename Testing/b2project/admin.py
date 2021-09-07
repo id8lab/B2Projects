2e41112702dcd8b4e15a40a5acfa20e9c0fb6c62
Ady <yethihaaung@my.jcu.edu.au>
2021-09-07 16:58:21 +0700

    Completed Contract Creation and JobLists

--- Testing/b2project/admin.py
+++ Testing/b2project/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from . models  import Feature 
 from . models import Contract
-<<<<<<< HEAD
 from . models import UserProfile
-=======
->>>>>>> 2747631b43589992519fc4336e12ef81ea50be47
 
 # Register your models here.
 # After created Database from models, import Database here and Register here, 
@@ -13,7 +10,4 @@ from . models import UserProfile
 
 admin.site.register(Feature)
 admin.site.register(Contract)
-<<<<<<< HEAD
 admin.site.register(UserProfile)
-=======
->>>>>>> 2747631b43589992519fc4336e12ef81ea50be47
